a0ace6f2aab685c1e4aeb71be0a0db9b88af2553
FKobus <ferry@famkob.us>
2017-04-02 14:20:41 +0200

    Added recipes and bash to php container

--- run.sh
+++ run.sh
@@ -59,7 +59,7 @@ while [[ $# -gt 0 ]] ; do
 done
 
 
-
+# Get the current direcotry name (for example development)
 function get_directory() {
 	current_directory=`pwd`
 	directories=(${current_directory//// })
@@ -87,21 +87,25 @@ Commands:
     up                        Will bring the services up
     down                      Shutsdown all services
     ssh                       Directly login to the php container
+    mysql                     Directly login to the mysql container
 EOF
 }
 
+##
+# Select the webserver when installing and
+##
 function choose_webserver() {
-	read -p 'Should we setup apache or nginx (apache|nginx): ' webserver
-	if [[ "$webserver" == "apache" || "$webserver" == "nginx" ]] ; then
-		echo "WEBSERVER=${webserver}" >> $CONFIG_FILE
-		FILES+=" -f docker-compose.${webserver}.yml"
+	read -p 'Should we setup apache or nginx (apache|nginx): ' WEBSERVER
+	if [[ "$WEBSERVER" == "apache" || "$WEBSERVER" == "nginx" ]] ; then
+		echo "WEBSERVER=${WEBSERVER}" >> $CONFIG_FILE
+		FILES+=" -f docker-compose.${WEBSERVER}.yml"
 	else
 		echo $INVALID
 		choose_webserver
 	fi
 }
 
-function choose_project() {
+function choose_project_name() {
 	read -p 'What name would you like for your environment?: ' environment
 	if [ -z "$environment" ] ; then
 		PROJECT="devenv"
@@ -118,24 +122,32 @@ function use_elastic() {
 	fi
 }
 
+function setup_hostname() {
+	read -p 'Hostname: ' DOMAINNAME
+	if [ -z "$DOMAINNAME" ] ; then
+		echo $INVALID
+		setup_hostname
+	fi
+}
+
 function rand() {
 	export LC_CTYPE=C
 	t=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
 }
 
 function get_database_config() {
-	read -p 'MySQL root password: ' db_root
-	if [ -z "$db_root" ] ; then
+	read -p 'MySQL root password: ' DB_ROOT
+	if [ -z "$DB_ROOT" ] ; then
 		rand
-		db_root=$t
-		echo "Generated root password: ${db_root}"
+		DB_ROOT=$t
+		echo "Generated root password: ${DB_ROOT}"
 	fi
 }
 
 function set_webserver() {
-	if [[ -z "$webserver" ]] ; then
-		webserver=`cat .config | grep WEBSERVER=`
-		webserver=${webserver:10}
+	if [[ -z "$WEBSERVER" ]] ; then
+		WEBSERVER=`cat .config | grep WEBSERVER=`
+		WEBSERVER=${WEBSERVER:10}
 	fi
 }
 
@@ -148,32 +160,34 @@ function set_project() {
 
 function setup_database() {
 	get_directory
-	read -p 'MySQL database user: ' db_user
-	if [ -z "$db_user" ] ; then
+
+	read -p 'MySQL database user: ' DB_USER
+	if [ -z "$DB_USER" ] ; then
 		rand
-		db_user="${current_directory}_user_${t:0:5}"
-		echo "Generated database user: ${db_user}"
+		DB_USER="${current_directory}_user_${t:0:5}"
+		echo "Generated database user: ${DB_USER}"
 	fi
-	read -p 'MySQL database name: ' db_name
-	if [ -z "$db_name" ] ; then
+	read -p 'MySQL database name: ' DB_NAME
+	if [ -z "$DB_NAME" ] ; then
 		rand
-		db_name="${current_directory}_name_${t:0:5}"
-		echo "Generated database name: ${db_name}"
+		DB_NAME="${current_directory}_name_${t:0:5}"
+		echo "Generated database name: ${DB_NAME}"
 	fi
-	read -p 'MySQL database password: ' db_pass
-	if [ -z "$db_pass" ] ; then
+	read -p 'MySQL database password: ' DB_PASS
+	if [ -z "$DB_PASS" ] ; then
 		rand
-		db_pass=$t
-		echo "Generated database password: ${db_pass}"
+		DB_PASS=$t
+		echo "Generated database password: ${DB_PASS}"
 	fi
 }
 
+# Get the ip of the webserver for aliasing
 function get_ip() {
 	# Loop over all running docker containers and find our chosen webserver
 	for service in `docker ps -q`; do
 		# Extract the servicename
 		servicename=`docker inspect --format '{{ .Name }}' $service`
-		validservice="${PROJECT}_${webserver}_"
+		validservice="${PROJECT}_${WEBSERVER}_"
 		if [[ ${servicename:1} == ${validservice}* ]] ; then
 			IP=`docker inspect --format {{.NetworkSettings.Networks.${PROJECT}_server.IPAddress}} $service`
 			break
@@ -210,6 +224,100 @@ function update_hosts_file() {
 		fi
 	fi
 }
+
+##
+# Installs a new environment from scratch
+# Check if we already runned the install
+# If not, continue with the installation
+##
+function do_install() {
+	FIRST_RUN=true
+	choose_project_name
+	choose_webserver
+	use_elastic
+	echo "FILES=${FILES}" >> $CONFIG_FILE
+	get_database_config
+	cat ".env.dist" > $TEMP_ENV_FILE
+	sed -i "" "s/^DB_ROOT_PASSWORD=.*/DB_ROOT_PASSWORD=${DB_ROOT}/g" $TEMP_ENV_FILE
+	add_project
+}
+
+##
+# Setup the project root and add vhosts
+##
+function setup_recipe() {
+	echo "Which type of project do you want to add?"
+	RECIPES=()
+	for entry in "recipes"/* ; do
+		RECIPES+=(${entry:8})
+	done
+
+	select RECIPE in "${RECIPES[@]}" ; do
+		if [[ "${RECIPES[@]}" =~ "${RECIPE}" ]]; then
+			break
+		else
+			echo $INVALID
+		fi
+	done
+
+	## copy web contents for flying start
+	cp -R recipes/${RECIPE}/web/ projects/${DOMAINNAME}
+
+	## remove .gitkeep if it exists
+	if [ -f "projects/${DOMAINNAME}/.gitkeep" ] ; then
+		rm projects/${DOMAINNAME}/.gitkeep
+	fi
+
+	## set te new vhost
+	if [ "$WEBSERVER" == "apache" ] ; then
+		VHOST="services/apache/vhosts/${DOMAINNAME}.conf"
+	elif [ "$WEBSERVER" == "nginx" ] ; then
+		VHOST="services/nginx/config/conf.d/${DOMAINNAME}.conf"
+	fi
+
+	## rewrite contents
+	cat recipes/${RECIPE}/config/${WEBSERVER}.${RECIPE}.conf > $VHOST
+	sed -i "" "s/domain.tld/${DOMAINNAME}/g" $VHOST
+	sed -i "" "s#/projects/project#/projects/${DOMAINNAME}#g" $VHOST
+
+	## Execute recipe specific operations
+	if [ "$RECIPE" == "wordpress" ] ; then
+		setup_wordpress
+	elif [ "$RECIPE" == "symfony" ] ; then
+		echo "NOT IMPLEMENTED YET"
+		exit
+	else
+		echo "NOT IMPLEMENTED YET"
+		exit
+	fi
+
+}
+
+function setup_wordpress() {
+	docker exec -i ${PROJECT}_php_1 /bin/bash <<EOF
+cd /var/www/projects/$DOMAINNAME
+composer create-project roots/bedrock .
+exit
+EOF
+}
+
+##
+# Add a project
+##
+function add_project() {
+	echo "Adding a new project to the stack"
+	setup_hostname
+	# Setup the database
+	setup_database
+	if [ "$FIRST_RUN" == true ] ; then
+		sed -i "" "s/^DB_NAME=.*/DB_NAME=${DB_NAME}/g" $TEMP_ENV_FILE
+		sed -i "" "s/^DB_USERNAME=.*/DB_USERNAME=${DB_USER}/g" $TEMP_ENV_FILE
+		sed -i "" "s/^DB_PASSWORD=.*/DB_PASSWORD=${DB_PASS}/g" $TEMP_ENV_FILE
+		mv $TEMP_ENV_FILE ".env"
+	fi
+	setup_recipe
+}
+
 
 ##
 # Bring the service up
@@ -250,76 +358,9 @@ function get_it_down() {
 	fi
 }
 
-function setup_hostname() {
-	read -p 'Hostname: ' domainname
-	if [ -z "$domainname" ] ; then
-		echo $INVALID
-		setup_hostname
-	fi
-}
-
 ##
-# Installs a new environment from scratch
-# Check if we already runned the install
-# If not, continue with the installation
+# Get into a specific service
 ##
-function do_install() {
-	FIRST_RUN=true
-	choose_project
-	choose_webserver
-	use_elastic
-	echo "FILES=${FILES}" >> $CONFIG_FILE
-	get_database_config
-	cat ".env.dist" > $TEMP_ENV_FILE
-	sed -i "" "s/^DB_ROOT_PASSWORD=.*/DB_ROOT_PASSWORD=${db_root}/g" $TEMP_ENV_FILE
-	add_project
-}
-
-function setup_apache_project() {
-	VHOST="services/apache/vhosts/${domainname}.conf"
-	cat services/apache/vhosts/example.config > $VHOST
-	create_project
-}
-
-function setup_nginx_project() {
-	VHOST="services/nginx/config/conf.d/${domainname}.conf"
-	cat services/nginx/config/conf.d/example.config > $VHOST
-	create_project
-}
-
-function create_project() {
-	sed -i "" "s/domain.tld/${domainname}/g" $VHOST
-	sed -i "" "s#/projects/project#/projects/${domainname}#g" $VHOST
-	cp -R templates/default projects/${domainname}
-}
-
-##
-# Add a project
-##
-function add_project() {
-	echo "Adding a new project to the stack"
-
-	setup_hostname
-
-	# Setup the database
-	setup_database
-
-	if [ "$FIRST_RUN" == true ] ; then
-		sed -i "" "s/^DB_NAME=.*/DB_NAME=${db_name}/g" $TEMP_ENV_FILE
-		sed -i "" "s/^DB_USERNAME=.*/DB_USERNAME=${db_user}/g" $TEMP_ENV_FILE
-		sed -i "" "s/^DB_PASSWORD=.*/DB_PASSWORD=${db_pass}/g" $TEMP_ENV_FILE
-		mv $TEMP_ENV_FILE ".env"
-	fi
-
-	if [ "$webserver" == "apache" ] ; then
-		setup_apache_project
-	fi
-
-	if [ "$webserver" == "nginx" ] ; then
-		setup_nginx_project
-	fi
-}
-
 function get_it_in() {
 	# Loop over all running docker containers and find our chosen webserver
 	for service in `docker ps -q`; do
@@ -336,6 +377,7 @@ function get_it_in() {
 	fi
 }
 
+## Always check the config!
 function check_config() {
 	# check if .config exists
 	if [ ! -f ".config" ] ; then
@@ -357,6 +399,10 @@ elif [ "$COMMAND" == "down" ] ; then
 	get_it_down
 elif [ "$COMMAND" == "add" ] ; then
 	check_config
+	CONTAINER_NAME="${PROJECT}_php_1"
+	if ! docker top $CONTAINER &>/dev/null ; then
+		get_it_up
+	fi
 	add_project
 elif [ "$COMMAND" == "ssh" ] ; then
 	check_config
@@ -379,7 +425,7 @@ fi
 ##
 
 # docker exec -i CONTAINER_NAME /bin/sh <<'EOF'
-# cd /var/www/projects/$domainname
+# cd /var/www/projects/$DOMAINNAME
 # rm -rf *
 # composer create-project roots/bedrock .
 # exit
